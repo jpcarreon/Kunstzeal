f3eaa4fa79369b5d577dd7ac4836dbb1fe82840f
John Philip Carreon <jccarreon3@up.edu.ph>
2023-02-27 19:42:45 +0800

    feat: improve dataset creation script
    * spectrograms now have more organized filenames
    * spectrograms will no longer be segregated by folders
    * script now generates a classification record in csv format

--- create_dataset.py
+++ create_dataset.py
@@ -1,15 +1,31 @@
 import os
 import spectrogram as sg
 
-count = 1
-inputPath = ".\input"
-
 print("Generating Spectrogram Data Set...\n")
 
+classification = {
+    "FLAC": 0, "V0": 1, "320K": 2,
+    "192K": 3, "128K": 4
+}
+inputPath = ".\input"
+outputPath = ".\data_output"
+
+if not os.path.exists(os.path.join(outputPath, "spectrograms")): 
+    os.makedirs(os.path.join(outputPath, "spectrograms"))
+
+dataset_csv = open(f"{outputPath}\spectral_record.csv", "w")
+
 for folder in os.listdir(inputPath):
+    counter = 0
     for fp in os.listdir(os.path.join(inputPath, folder)):
-        print(f"Current: {folder}, {fp}")
+        print(f"Current: [{folder}] {fp}")
 
         currentFile = os.path.join(inputPath, folder, fp)
-        savePath = os.path.join(".\data_output", folder, fp[:-4]) + ".png"
+        savePath = os.path.join(outputPath, "spectrograms", f"{folder}_{counter}.png")
+        
         sg.saveSpectrogram(currentFile, savePath, size_x = 2.56, size_y = 2.56)
+        dataset_csv.write(f"{folder}_{counter}.png, {classification[folder]}")
+
+        counter += 1
+
+dataset_csv.close()
