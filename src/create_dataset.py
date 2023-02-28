85f022b8232e63ecf53aa5603173260122dce239
John Philip Carreon <jccarreon3@up.edu.ph>
2023-02-28 13:58:04 +0800

    refactor: format python files and fix dir paths
    Signed-off-by: John Philip Carreon <jccarreon3@up.edu.ph>

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -7,13 +7,13 @@ classification = {
     "FLAC": 0, "V0": 1, "320K": 2,
     "192K": 3, "128K": 4
 }
-inputPath = ".\input"
-outputPath = ".\data_output"
+inputPath = "./input"
+outputPath = "./data_output"
 
-if not os.path.exists(os.path.join(outputPath, "spectrograms")): 
+if not os.path.exists(os.path.join(outputPath, "spectrograms")):
     os.makedirs(os.path.join(outputPath, "spectrograms"))
 
-dataset_csv = open(f"{outputPath}\spectral_record.csv", "w")
+dataset_csv = open(f"{outputPath}/spectral_record.csv", "w")
 
 for folder in os.listdir(inputPath):
     counter = 0
@@ -22,8 +22,8 @@ for folder in os.listdir(inputPath):
 
         currentFile = os.path.join(inputPath, folder, fp)
         savePath = os.path.join(outputPath, "spectrograms", f"{folder}_{counter}.png")
-        
-        sg.saveSpectrogram(currentFile, savePath, size_x = 2.56, size_y = 2.56)
+
+        sg.saveSpectrogram(currentFile, savePath, size_x=2.56, size_y=2.56)
         dataset_csv.write(f"{folder}_{counter}.png, {classification[folder]}\n")
 
         counter += 1
--- src/spectrogram.py
+++ src/spectrogram.py
@@ -9,38 +9,40 @@ spek_cmap = mpl.colors.LinearSegmentedColormap.from_list(
     ["black", "indigo", "blue", "cyan", "chartreuse", "yellow", "orange", "red"]
 )
 
+
 def __loadAudioFile(filepath, n_fft, win_size, hop_size, amin, top_db):
-    amp, sr = librosa.load(filepath, sr = None)
+    amp, sr = librosa.load(filepath, sr=None)
     stft = librosa.stft(
-        amp, 
-        n_fft = n_fft,
-        win_length = win_size,
-        hop_length = hop_size
+        amp,
+        n_fft=n_fft,
+        win_length=win_size,
+        hop_length=hop_size
     )
     dB = librosa.amplitude_to_db(
-        np.abs(stft), 
-        ref = np.max,
-        amin = amin,
-        top_db = top_db
+        np.abs(stft),
+        ref=np.max,
+        amin=amin,
+        top_db=top_db
     )
 
     return (dB, sr)
 
+
 def saveSpectrogram(
-    filepath, 
-    outputPath, 
-    n_fft = 2048, 
-    win_size = 2048, 
-    hop_size = 512, 
-    amin = 0.00184, 
-    top_db = 120,
-    size_x = 5,
-    size_y = 3
+    filepath,
+    outputPath,
+    n_fft=2048,
+    win_size=2048,
+    hop_size=512,
+    amin=0.00184,
+    top_db=120,
+    size_x=5,
+    size_y=3
 ):
     dB, sr = __loadAudioFile(filepath, n_fft, win_size, hop_size, amin, top_db)
 
-    fig, ax = plt.subplots(figsize = (size_x, size_y))
-    img = specshow(dB, sr = sr, ax = ax, x_axis = "time", y_axis = "hz", cmap = spek_cmap)
+    fig, ax = plt.subplots(figsize=(size_x, size_y))
+    img = specshow(dB, sr=sr, ax=ax, x_axis="time", y_axis="hz", cmap=spek_cmap)
 
     plt.axis("off")
     fig.tight_layout(pad=0)
@@ -48,21 +50,22 @@ def saveSpectrogram(
     plt.clf()
     plt.close()
 
+
 def displaySpectrogram(
-    filepath, 
-    n_fft = 2048, 
-    win_size = 2048, 
-    hop_size = 512, 
-    amin = 0.00184, 
-    top_db = 120,
-    size_x = 5,
-    size_y = 3
+    filepath,
+    n_fft=2048,
+    win_size=2048,
+    hop_size=512,
+    amin=0.00184,
+    top_db=120,
+    size_x=5,
+    size_y=3
 ):
     dB, sr = __loadAudioFile(filepath, n_fft, win_size, hop_size, amin, top_db)
 
-    fig, ax = plt.subplots(figsize = (size_x, size_y))
-    img = specshow(dB, sr = sr, ax = ax, x_axis = "time", y_axis = "hz", cmap = spek_cmap)
+    fig, ax = plt.subplots(figsize=(size_x, size_y))
+    img = specshow(dB, sr=sr, ax=ax, x_axis="time", y_axis="hz", cmap=spek_cmap)
 
-    fig.colorbar(img, ax = ax, format = f"%0.2f dB")
+    fig.colorbar(img, ax=ax, format=f"%0.2f dB")
     fig.tight_layout()
     plt.show()
