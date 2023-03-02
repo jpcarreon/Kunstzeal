d453a0d23e0101e744d016d9f311d21eecc8a503
John Philip Carreon <jccarreon3@up.edu.ph>
2023-03-02 09:23:52 +0800

    feat: add CNN sample that uses CIFAR10 data

---
+++ src/cnn.py
@@ -0,0 +1,132 @@
+# Following directions from https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+num_epochs = 2
+batch_size = 4
+learning_rate = 0.001
+momentum = 0.9
+
+# dataset has PILImage images of range [0, 1]. 
+# We transform them to Tensors of normalized range [-1, 1]
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+# CIFAR10: 60000 32x32 color images in 10 classes, with 6000 images per class
+train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+
+test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+dataiter = iter(train_loader)
+images, labels = next(dataiter)
+
+# show images & labels
+#print(" ".join(f"{classes[labels[j]]:5s}" for j in range(batch_size)))
+#imshow(torchvision.utils.make_grid(images))
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # -> n, 3, 32, 32
+        x = self.pool(F.relu(self.conv1(x)))  # -> n, 6, 14, 14
+        x = self.pool(F.relu(self.conv2(x)))  # -> n, 16, 5, 5
+        x = torch.flatten(x, 1)               # -> n, 400
+        x = F.relu(self.fc1(x))               # -> n, 120
+        x = F.relu(self.fc2(x))               # -> n, 84
+        x = self.fc3(x)                       # -> n, 10
+        return x
+
+net = ConvNet().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+
+# iterate through epochs
+for epoch in range(num_epochs):
+    running_loss = 0.0
+
+    # iterate through each batch
+    for i, data in enumerate(train_loader, 0):
+        inputs = data[0].to(device)
+        labels = data[1].to(device)
+
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        if (i + 1) % 2000 == 0:
+            print(f"[Epoch {epoch + 1}, Step {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+            running_loss = 0.0
+
+print("Finished Training")
+torch.save(net.state_dict(), "./cifar_net.pth")
+
+
+dataiter = iter(test_loader)
+images, labels = next(dataiter)
+
+net = ConvNet()
+net.load_state_dict(torch.load("./cifar_net.pth"))
+
+correct_pred = {classname: 0 for classname in classes}
+total_pred = {classname: 0 for classname in classes}
+
+with torch.no_grad():
+    for data in test_loader:
+        images, labels = data
+
+        outputs = net(images)
+
+        _, predictions = torch.max(outputs, 1)
+
+        for label, predictions in zip(labels, predictions):
+            if label == predictions:
+                correct_pred[classes[label]] += 1
+            total_pred[classes[label]] += 1
+
+for classname, count in correct_pred.items():
+    accuracy = 100 * float(count) / total_pred[classname]
+    print(f"Accuracy [{classname:5s}]: {count} / {total_pred[classname]} = {accuracy:.1f}%")
