f019bf8cfaf1abb3ab122ea94fa7e47765952303
John Philip Carreon <jccarreon3@up.edu.ph>
2023-03-02 18:29:47 +0800

    feat: implement CNN for spectrogram data

--- src/cnn.py
+++ src/cnn.py
@@ -8,28 +8,28 @@ import cnn_utils
 class ConvNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        nn.Conv2d(3,6, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.conv1 = nn.Conv2d(3, 32, 5)
+        self.conv2 = nn.Conv2d(32, 32, 5)
+        self.fc1 = nn.Linear(32 * 28 * 28, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 5)
 
     def forward(self, x):
-        # -> n, 3, 32, 32
-        x = self.pool(F.relu(self.conv1(x)))  # -> n, 6, 14, 14
-        x = self.pool(F.relu(self.conv2(x)))  # -> n, 16, 5, 5
-        x = torch.flatten(x, 1)               # -> n, 400
-        x = F.relu(self.fc1(x))               # -> n, 120
-        x = F.relu(self.fc2(x))               # -> n, 84
-        x = self.fc3(x)                       # -> n, 10
+        # -> n, 3, 256, 256
+        x = self.pool(F.relu(self.conv1(x)))  # -> n, 32, 126, 126
+        x = self.pool(F.relu(self.conv2(x)))  # -> n, 32, 61, 61
+        x = self.pool(F.relu(self.conv2(x)))  # -> n, 32, 28, 28
+        x = torch.flatten(x, 1)               # -> n, 25088
+        x = F.relu(self.fc1(x))               # -> n, 128
+        x = F.relu(self.fc2(x))               # -> n, 128
+        x = self.fc3(x)                       # -> n, 5
         return x
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-num_epochs = 2
+num_epochs = 30
 batch_size = 4
 learning_rate = 0.001
 momentum = 0.9
@@ -52,3 +52,31 @@ test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
 # cnn_utils.viewRandomSample(train_loader, classes, batch_size)
 
 net = ConvNet().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+
+step_size = ((len(train_set) + 1) // batch_size) // 4
+
+for epoch in range(num_epochs):
+    running_loss = 0.0
+
+    for i, data in enumerate(train_loader, 0):
+        inputs = data[0].to(device)
+        labels = data[1].to(device)
+
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+        if (i + 1) % step_size == 0:
+            print(f"[Epoch {epoch + 1}, Step {i + 1:5d}] loss: {running_loss / 2000:.3f}")
+            running_loss = 0.0
+
+print("Finished Training")
+torch.save(net.state_dict(), "./specral_net.pt")
