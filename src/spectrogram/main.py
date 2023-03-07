5a96bdaf4f7e92061d73498b441b1c8d0a3d6104
John Philip Carreon <jccarreon3@up.edu.ph>
2023-03-07 08:25:32 +0800

    refactor: organize python files

--- src/spectrogram/main.py
+++ src/spectrogram/main.py
@@ -2,5 +2,5 @@ import spectrogram as sg
 
 
 #sg.displaySpectrogram("./samples/sample.wav", size_x=10, size_y=6)
-sg.saveSpectrogram("./samples/sample.wav", "./output/output.png", size_x=2.56, size_y=2.56)
+sg.saveSpectrogram("sample.mp3", "./output/output.png", size_x=2.56, size_y=2.56)
 
